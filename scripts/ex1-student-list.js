18a0c9edc87f83867073b5fc005826844ba1ee6e
Giga Pok <gigapokera@gmail.comm>
2021-04-18 21:39:13 +0400

    finished project

--- scripts/ex1-student-list.js
+++ scripts/ex1-student-list.js
@@ -1,16 +1,12 @@
 const table = document.getElementById('table');
+const sub = document.getElementById('subject');
+const formWrap = document.querySelector('#form div');
 
 let html = '';
 
-let subject = ['', 'html', 'css', 'js', 'react', 'angular', 'Average'];
+let subjects = ['', 'html', 'css', 'js', 'react', 'angular'];
 
-html += '<tr>';
-for (i = 0; i < subject.length; i++) {
-    html += `<th>${subject[i]}</th>`;
-}
-html += '</tr>';
-
-const students = [
+let students = [
     {
         name: 'nika',
         grades: [12, 42, 93, 74, 35]
@@ -28,32 +24,112 @@ const students = [
         grades: [21, 52, 13, 16, 23]
     }
 ];
-for (i = 0; i < students.length; i++) {
-    const student = students[i];
-    let average = 0;
+
+function studentsGenerate() {
 
     html += '<tr>';
-    html += `<td>${student.name}</td>`;
-
-    for (a = 0; a < student.grades.length; a++) {
-        html += `<td>${student.grades[a]}</td>`;
+    for (i = 0; i < subjects.length; i++) {
+        html += `<th>${subjects[i]}</th>`;
     }
-    for (e = 0; e < student.grades.length; e++){
-        average += student.grades[e];
-    }
-    average /= student.grades.length;
-
-    if(average <= 50){
-        html += `<td class="red">${average}</td>`;
-    } else {
-        html += `<td class="green">${average}</td>`;
-    }
-
-    // html += `<td>${student.grades.reduce((a, b) => a + b, 0) / student.grades.length}</td>`;
 
     html += '</tr>';
 
+    let sum = Array(subjects.length - 1).fill(0);
+    console.log(sum);
+    for (i = 0; i < students.length; i++) {
+        let student = students[i];
+
+        html += '<tr>';
+        html += `<td>${student.name}</td>`;
+
+        for (j = 1; j < subjects.length; j++) {
+            const grade = student.grades[j - 1] || 0;
+            sum[j - 1] += grade;
+
+            if (i === students.length - 1) {
+                sum[j - 1] = Math.round(sum[j - 1] / students.length);
+            }
+            html += `<td class= "${grade > 50 ? "green" : "red"}">${grade}</td>`;
+        }
+
+        // html += `<td>${student.grades.reduce((a, b) => a + b, 0) / student.grades.length}</td>`;
+
+        html += '</tr>';
+
+    }
+    average(sum);
+    console.log(sum);
+    table.innerHTML = html;
 }
 
-table.innerHTML = html;
+function addStudent(event) {
+    event.preventDefault();
+    let formInput = document.querySelectorAll('#form input');
+    console.log(formInput);
+    let newStudent = {
+        name: '',
+        grades: []
+    };
+
+    for (let i = 0; i < formInput.length; i++) {
+        if (formInput[i].name === 'name') {
+            newStudent.name = formInput[i].value;
+        } else {
+            newStudent.grades.push(+formInput[i].value);
+        }
+    }
+    console.log(newStudent);
+    students.push(newStudent);
+    html = " ";
+    studentsGenerate();
+}
+
+function average(average) {
+
+    console.log();
+
+    html += '<tr>';
+    html += `<td>Average</td>`;
+
+    for (i = 0; i < average.length; i++) {
+        html += `<td>${average[i]}</td>`;
+    }
+
+    html += '</tr>';
+
+    table.innerHTML = html;
+
+}
+
+studentsGenerate();
+generateInput();
+
+function addSubject(event) {
+    event.preventDefault();
+    subjects.push(sub.value);
+    html = "";
+    studentsGenerate();
+    generateInput();
+}
+
+function generateInput() {
+    let input = 
+    `<div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+    </div>
+    `;
+
+    for (let i = 1; i < subjects.length; i++) {
+        input += 
+    `
+    <div>
+        <label for="number${i}">${subjects[i]}</label>
+        <input type="number" id="number${i}">
+    </div>
+    `
+    }
+
+    formWrap.innerHTML = input;
+}
 
