909e7a19755f1e981d63fa8f57aa2ef421806472
Giga Pok <gigapokera@gmail.comm>
2021-05-14 13:57:27 +0400

    slider

--- component.js
+++ component.js
@@ -19,3 +19,36 @@ function burger() {
         count = 0;
     }
 }
+
+// სლაიდერი
+
+var slides = document.getElementsByClassName("mySlides");
+
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+
+    slides[slideIndex - 1].classList.add('active')
+}
