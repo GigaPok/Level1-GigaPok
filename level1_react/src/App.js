d1355ecfe21c0b33f9e677d8dda378a1ea426f14
Giga Pok <gigapokera@gmail.comm>
2021-06-09 18:18:26 +0400

    finish

--- level1_react/src/App.js
+++ level1_react/src/App.js
@@ -1,9 +1,11 @@
+import './reset.scss'
 import './App.css';
+import Todo from './Todo/Todo'
 
 function App() {
   return (
     <div className="App">
-      
+      <Todo />
     </div>
   );
 }
---
+++ level1_react/src/Todo/Todo.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+
+    const [data, setData] = useState([]);
+    const [tittle, setTitle] = useState('');
+    let handleChange = (e) => {
+        setTitle(e.target.value);
+    }
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        setTitle("");
+        setData([
+
+            {
+                tittle: tittle,
+                checked: false
+            },
+            ...data
+
+        ]
+        )
+    }
+    let handleDelete = (index) => {
+        let tmp = [...data]
+        tmp.splice(index, 1)
+
+        setData([
+            ...tmp
+        ]
+        )
+    }
+    let handleStatus = (index) => {
+        let tmp = [...data];
+        tmp[index].checked = !tmp[index].checked;
+        setData(tmp);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input placeholder='Enter' value={tittle}
+                    onChange={handleChange}></input>
+                <button type='submit'>Submit</button>
+            </form>
+            <ul>
+                {data.map((el, index) => {
+                    return (
+                        <li key={index}>
+                            <input type='checkbox' checked={el.checked}
+                                onChange={() => { handleStatus(index) }}></input>
+                            {el.tittle}
+                            <button onClick={() => { handleDelete(index) }}>Del</button>
+                        </li>)
+                }
+                )
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default Todo;
